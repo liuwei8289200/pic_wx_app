156ac99c13cea27c5e68572e76a52520f27ca8a8
liuwei8289200 <326968716@qq.com>
2024-12-25 01:18:19 +0800

    update

--- local_file/pages/index/index.js
+++ local_file/pages/index/index.js
@@ -1,12 +1,16 @@
 
 import { getHeightImages } from '../../utils/image'
 import { getAlbum } from '../../utils/store'
+import { installRouteBuilder } from './route'
+import { generateGridList, compareVersion } from './utils'
 
+const { screenWidth } = wx.getSystemInfoSync()
 const descList = [
     '这里风景好美～',
     '这是哪里呀？快介绍一下～～～～'
 ]
-Page({
+Page({});
+Component({
   data: {
     swiperImages: [],
     imageList: [], // 瀑布流图片
@@ -20,79 +24,100 @@ Page({
     imageMargin: 12, // 图片间距
     lineLimit: 3, // 每行多少张图片
     list: [],
-  },
-  onLoad() {
-    this.loadSwiperImages();
-    this.setData({
-      list: this.data.list.concat(getnewList())
-    })
-    const { imageMargin, lineLimit } = this.data
-    const { screenWidth } = wx.getSystemInfoSync()
-    this.setData({
-      imageWidth: (screenWidth - imageMargin * 4) / lineLimit, // 图片宽度
-      list: getAlbum(),
-    })
-  },
-  onShow() {
-    setTimeout(() => {
-      console.log(213213213131311232131231)
-      this.getAlbumHeight();
-    }, 1000);
-  },
-  loadSwiperImages() {
-    wx.cloud.callFunction({
-      name: 'getFiles',
-      data: {
-        directoryPath: 'swiper_images/' // 替换为您的文件夹路径
-      },
-      success: res => {
-        console.log('success', res);
-        console.log('云函数调用成功：', res.result);
-        console.log("test1111111", res.result.data);
-        //获取到size>0的后,并将其中的Key值存入数组
-        //将数组中的文件名改为cloud://6d69-mini-program-8gte1ziw1d4ac40c-1258427370/swiper_images/1.png
-        const pngFiles = res.result.data.filter(file => file.Size > 0 ).map(file => `cloud://mini-program-8gte1ziw1d4ac40c.6d69-mini-program-8gte1ziw1d4ac40c-1258427370/${file.Key}`);
-        console.log(pngFiles);
-        wx.cloud.getTempFileURL({
-          fileList: pngFiles,
-          success: res => {
-            // fileList 是一个有如下结构的对象数组
-            // [{
-            //    fileID: 'cloud://xxx.png', // 文件 ID
-            //    tempFileURL: '', // 临时文件网络链接
-            //    maxAge: 120 * 60 * 1000, // 有效期
-            // }]
-            console.log(res.fileList)
-            this.setData({
-              swiperImages: res.fileList.map(file => file.tempFileURL)
-            });
-            console.log(this.data.swiperImages);
-          },
-          fail: console.error
-        })
-      },
-      fail: err => {
-        console.error('云函数调用失败：', err);
-      }
-    });
 
+    padding: 4,
+    gridList: generateGridList(100, 2),
+    cardWidth: (screenWidth - 4 * 2 - 4) / 2, // 减去间距
   },
-  loadMoreImages() {
-    this.loadWaterfallImages();
+  methods:{
+    onLoad() {
+      this.loadSwiperImages();
+      this.setData({
+        list: this.data.list.concat(getnewList())
+      })
+      const { imageMargin, lineLimit } = this.data
+      const { screenWidth } = wx.getSystemInfoSync()
+      this.setData({
+        imageWidth: (screenWidth - imageMargin * 4) / lineLimit, // 图片宽度
+        list: getAlbum(),
+      })
+    },
+    onShow() {
+      setTimeout(() => {
+        console.log(213213213131311232131231)
+        this.getAlbumHeight();
+      }, 1000);
+    },
+    loadSwiperImages() {
+      wx.cloud.callFunction({
+        name: 'getFiles',
+        data: {
+          directoryPath: 'swiper_images/' // 替换为您的文件夹路径
+        },
+        success: res => {
+          console.log('success', res);
+          console.log('云函数调用成功：', res.result);
+          console.log("test1111111", res.result.data);
+          //获取到size>0的后,并将其中的Key值存入数组
+          //将数组中的文件名改为cloud://6d69-mini-program-8gte1ziw1d4ac40c-1258427370/swiper_images/1.png
+          const pngFiles = res.result.data.filter(file => file.Size > 0 ).map(file => `cloud://mini-program-8gte1ziw1d4ac40c.6d69-mini-program-8gte1ziw1d4ac40c-1258427370/${file.Key}`);
+          console.log(pngFiles);
+          wx.cloud.getTempFileURL({
+            fileList: pngFiles,
+            success: res => {
+              // fileList 是一个有如下结构的对象数组
+              // [{
+              //    fileID: 'cloud://xxx.png', // 文件 ID
+              //    tempFileURL: '', // 临时文件网络链接
+              //    maxAge: 120 * 60 * 1000, // 有效期
+              // }]
+              console.log(res.fileList)
+              this.setData({
+                swiperImages: res.fileList.map(file => file.tempFileURL)
+              });
+              console.log(this.data.swiperImages);
+            },
+            fail: console.error
+          })
+        },
+        fail: err => {
+          console.error('云函数调用失败：', err);
+        }
+      });
+  
+    },
+    loadMoreImages() {
+      this.loadWaterfallImages();
+    },
+    viewImageDetail(event) {
+      const imageUrl = event.currentTarget.dataset.url;
+      wx.navigateTo({
+        url: `/pages/detail/detail?imageUrl=${imageUrl}`
+      });
+    },
+    getAlbumHeight() {
+      wx.createSelectorQuery().select(".tab-bar").boundingClientRect(function (rect) {
+        console.log('asdsad', rect)
+      }).exec((res) => {
+        console.log(res)
+      })
+    },
   },
-  viewImageDetail(event) {
-    const imageUrl = event.currentTarget.dataset.url;
-    wx.navigateTo({
-      url: `/pages/detail/detail?imageUrl=${imageUrl}`
-    });
+  lifetimes: {
+    created() {
+      const {SDKVersion} = wx.getSystemInfoSync()
+      // console.log("test2222222", SDKVersion)
+      // console.log("test222222", this.cardWidth)
+      // console.log("test22222", this.gridList)
+      if (compareVersion(SDKVersion, '2.30.1') < 0) {
+        wx.showModal({
+          content: '基础库版本低于 v2.30.1 可能会有显示问题，建议升级微信体验。',
+          showCancel: false
+        })
+      }
+      installRouteBuilder()
+    },
   },
-  getAlbumHeight() {
-    wx.createSelectorQuery().select(".tab-bar").boundingClientRect(function (rect) {
-      console.log('asdsad', rect)
-    }).exec((res) => {
-      console.log(res)
-    })
-  }
 });
 function getnewList() {
   const newList = new Array(20).fill(0)

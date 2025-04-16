3d1bd152ada54498872eab74cb1b08fae2685b6c
liuwei8289200 <326968716@qq.com>
2025-04-17 00:32:18 +0800

    修改了云函数 在app.js获取openid的时候就检测一下 用户是否存在

--- local_file/app.js
+++ local_file/app.js
@@ -1,16 +1,9 @@
 // app.js
+const { init } = require("@cloudbase/wx-cloud-client-sdk");
+const client = init(wx.cloud);
+const models = client.models;
 App({
   onLaunch() {
-    // const that = this;
-    // // 获取系统信息
-    // const systemInfo = wx.getSystemInfoSync();
-    // // 胶囊按钮位置信息
-    // const menuButtonInfo = wx.getMenuButtonBoundingClientRect();
-    // // 导航栏高度 = 状态栏高度 + 44
-    // that.globalData.navBarHeight = systemInfo.statusBarHeight + 44;
-    // that.globalData.menuRight = systemInfo.screenWidth - menuButtonInfo.right;
-    // that.globalData.menuTop=  menuButtonInfo.top;
-    // that.globalData.menuHeight = menuButtonInfo.height;
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
@@ -31,19 +24,5 @@ App({
         console.log("Storage openId is ", wx.getStorageSync('openId'))
       }
     })
-    // 登录（现在这个已经作废了）
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //     console.log("test11111111", res.code)
-    //   }
-    // })
-  },
-  // globalData: {
-  //   userInfo: null,
-  //   navBarHeight: 0, // 导航栏高度
-  //   menuRight: 0, // 胶囊距右方间距（方保持左、右间距一致）
-  //   menuTop: 0, // 胶囊距底部间距（保持底部间距一致）
-  //   menuHeight: 0, // 胶囊高度（自定义内容可与胶囊高度保证一致）
-  // }
+  }
 })
